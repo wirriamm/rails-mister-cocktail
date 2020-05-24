84b0d857aaf17bde414af57b134f577bf14878be
William Goh <wirriamm@gmail.com>
2020-05-24 19:47:10 +0800

    remigrated db

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2020_05_24_085704) do
   enable_extension "plpgsql"
 
   create_table "cocktails", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
