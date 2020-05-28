47ee782c2f0882a93c60c6341b8b4d2c331ddcf4
William Goh <wirriamm@gmail.com>
2020-05-28 18:37:45 +0800

    routed root path to index as well (on top of /cocktails)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'doses/new'
+  root to: 'cocktails#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cocktails, only: [:index, :show, :new, :create] do
     # Don't need doses#index as doses can be listed in #cocktail#show
