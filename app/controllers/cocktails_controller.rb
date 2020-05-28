5e7b790de2842ad12638c5f73434a624d3593a58
William Goh <wirriamm@gmail.com>
2020-05-28 19:52:41 +0800

    added photo attribute to cocktails

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -23,6 +23,6 @@ class CocktailsController < ApplicationController
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :description)
+    params.require(:cocktail).permit(:name, :description, :photo)
   end
 end
